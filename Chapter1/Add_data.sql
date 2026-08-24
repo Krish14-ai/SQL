e33a446149503a2a0050fe78e1ee1599502def87
krish14-ai <kkhandelwal292@gmail.com>
2026-08-24 23:46:05 +0530

    Fixed errors in bday format

--- Chapter1/Add_data.sql
+++ Chapter1/Add_data.sql
@@ -1,9 +1,14 @@
--- To add data into Tabel
-Insert Into Students 
-values (1, "Krish", 20,'14-04-2006');
+-- Insert single record
+INSERT INTO Students (id, name, age, bday)
+VALUES (1, 'Krish', 20, '2006-04-14');
 
--- To add Multiple Entities in Tabel
-Insert Into Students
-Values (2,"Pallavi", 18,'23-06-2006'),(3,"Harshu", 21,'12-05,2005'),(4, "Sudi", -1,'1-1-2001'),(5, "Sandhya", 19,'1-2-2003');
+-- Insert multiple records
+INSERT INTO Students (id, name, age, bday)
+VALUES 
+    (2, 'Pallavi', 18, '2006-06-23'),
+    (3, 'Harshu', 21, '2005-05-12'),
+    (4, 'Sudi',  1,  '2001-01-01'),  -- fixed negative age
+    (5, 'Sandhya', 19, '2003-02-01');
 
-Select * from Students
+-- View all records
+SELECT * FROM Students;
