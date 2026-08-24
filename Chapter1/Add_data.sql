459e3f4770bb2640e8db640692330ab0a1526bbf
krish14-ai <kkhandelwal292@gmail.com>
2026-08-24 23:44:34 +0530

    Added everyone's bdays

--- Chapter1/Add_data.sql
+++ Chapter1/Add_data.sql
@@ -1,10 +1,9 @@
 -- To add data into Tabel
 Insert Into Students 
-values (1, "Krish", 20);
+values (1, "Krish", 20,'14-04-2006');
 
 -- To add Multiple Entities in Tabel
 Insert Into Students
-Values (2,"Pallavi", 18),(3,"Harshu", 21),(4, "Sudi", -1),(5, "Sandhya", 19);
+Values (2,"Pallavi", 18,'23-06-2006'),(3,"Harshu", 21,'12-05,2005'),(4, "Sudi", -1,'1-1-2001'),(5, "Sandhya", 19,'1-2-2003');
 
 Select * from Students
-
