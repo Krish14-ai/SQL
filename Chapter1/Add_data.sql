2e57263eb485b33d8eb66cbd37686c8b0a061d38
krish14-ai <kkhandelwal292@gmail.com>
2026-08-24 22:07:19 +0530

    adding data in Tabel

--- Chapter1/Add_data.sql
+++ Chapter1/Add_data.sql
@@ -1,9 +1,10 @@
--- adding a new column in Tabel
-Alter Table Stuedents
-add email Varchar(100)
+-- To add data into Tabel
+Insert Into Students 
+values (1, "Krish", 20);
 
+-- To add Multiple Entities in Tabel
+Insert Into Students
+Values (2,"Pallavi", 18),(3,"Harshu", 21),(4, "Sudi", -1),(5, "Sandhya", 19);
 
+Select * from Students
 
-
--- View all the Students
-select * from Students 
---
+++ Chapter1/Alter_Cols.sql
@@ -0,0 +1,10 @@
+-- adding a new column in Tabel
+Alter Table Stuedents
+add email Varchar(100)
+
+-- To Drop Columns in Tabel
+Alter Table Students
+drop column email
+
+-- View all the Students
+select * from Students 
