4f7136b477f0d1af8f390e2f46cad6d5da3862d7
krish14-ai <kkhandelwal292@gmail.com>
2026-08-29 17:24:39 +0530

    Came home after 2 days and learning SQL

---
+++ Chapter_2/start.sql
@@ -0,0 +1,28 @@
+-- sec_1
+---------------------------------------------------------------------
+CREATE DATABASE Chapter_2;
+
+USE Chapter_2; 
+
+CREATE TABLE students (
+    std_id VARCHAR(5) PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    sex VARCHAR(10) CHECK (sex IN ('male', 'female', 'other')),
+    contact_no VARCHAR(10),
+    email VARCHAR(100),
+    passed BOOLEAN
+);
+
+SELECT * FROM students;
+
+-------------------------------------------------------------------------
+
+-- Sec_2 
+
+use Chapter_2;
+Alter table students
+add Column date_of_birth DATE after age;
+
+use Chapter_2;
+select * from students
