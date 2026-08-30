75a0f65e48cdbfd05c623eac0c7160986bc4eed9
krish14-ai <kkhandelwal292@gmail.com>
2026-08-30 23:45:31 +0530

    Gave it a new look

--- Practice_Questions/Easy_problems.sql
+++ Practice_Questions/Easy_problems.sql
@@ -1,40 +1,65 @@
--- Active: 1787514242444@@127.0.0.1@3306@practice_questions
+-- =========================================================
+-- DATABASE SETUP
+-- =========================================================
 
-Create DATABASE Practice_Questions
+CREATE DATABASE Practice_Questions;
 
--- Q1) Display all students. 
+USE Practice_Questions;
 
-use Practice_Questions;  -- Using Students_ cuz i already have a Table named students in Chapter_2
-Create Table Students_new (
-    st_id varchar(10) PRIMARY KEY,
-    name Varchar(100),
+
+-- =========================================================
+-- TABLE SETUP
+-- =========================================================
+
+CREATE TABLE Students_new (
+    st_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100),
     age TINYINT UNSIGNED,
-    dob Date,
-    gender varchar(10)  CHECK (gender IN ('male', 'female', 'other')),
+    dob DATE,
+    gender VARCHAR(10) CHECK (gender IN ('male', 'female', 'other')),
     phone VARCHAR(10),
-    email Varchar(100),
+    email VARCHAR(100),
     marks TINYINT UNSIGNED
-
 );
 
-insert into Students_new
-values 
-(101, "krish", 20, '2006-04-14', 'male', '123456789', 'krish123@gmail.com', 98),
-(102, "Pallavi", 19, '2007-06-23', "female", "1234567890", "pallo@gmail.com",99),
-(103, "Harshu", 21, '2005-5-20', 'other', '0987654321','harshu@gmail.com',98),
-(104,"Sudi",12, '2006-01-01', 'female','1122334455', "chinti_sudi@gmail.com", 10),
-(105, "Sandy", 18, '2008-01-01', "female", '1111111111', "sandy@gmail.com", 98),
-(106, "Arpit", 19, '2007-02-02',"other",'2222222222',"arpit@gmail.com", 90);
 
-select * from students_new
+-- =========================================================
+-- INSERT DATA
+-- =========================================================
 
--------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------
+INSERT INTO Students_new
+VALUES 
+(101, 'Krish', 20, '2006-04-14', 'male', '123456789', 'krish123@gmail.com', 98),
+(102, 'Pallavi', 19, '2007-06-23', 'female', '1234567890', 'pallo@gmail.com', 99),
+(103, 'Harshu', 21, '2005-05-20', 'other', '0987654321', 'harshu@gmail.com', 98),
+(104, 'Sudi', 12, '2006-01-01', 'female', '1122334455', 'chinti_sudi@gmail.com', 10),
+(105, 'Sandy', 18, '2008-01-01', 'female', '1111111111', 'sandy@gmail.com', 98),
+(106, 'Arpit', 19, '2007-02-02', 'other', '2222222222', 'arpit@gmail.com', 90);
 
--- Q2) Display only the `name` column
 
-select name from students_new
--------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------
+-- =========================================================
+-- PRACTICE QUESTIONS
+-- =========================================================
 
--- Q3) 
+-- Q1) Display all students.
+
+SELECT *
+FROM Students_new;
+
+
+-- Q2) Display only the name column.
+
+SELECT name
+FROM Students_new;
+
+
+-- Q3) Display name and age.
+
+SELECT name, age
+FROM Students_new;
+
+
+-- Q4) Display name, gender, and marks.
+
+SELECT name, gender, marks
+FROM Students_new;
