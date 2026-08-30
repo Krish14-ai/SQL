0d3a993f79d7424d11f08c17bc91b7cc10e40c8f
krish14-ai <kkhandelwal292@gmail.com>
2026-08-30 16:30:26 +0530

    Started to go in details

--- Chapter_2/start.sql
+++ Chapter_2/start.sql
@@ -1,6 +1,7 @@
+-- SQLBook: Code
 -- sec_1
 ---------------------------------------------------------------------
-CREATE DATABASE Chapter_2;
+CREATE DATABASE If NOT EXISTS Chapter_2;
 
 USE Chapter_2; 
 
@@ -19,11 +20,23 @@ SELECT * FROM students;
 -------------------------------------------------------------------------
 
 -- Sec_2 
+--I will use this "Use Chapter_2" 
+--here often because i have another Table named students in Practice DB
 
 use Chapter_2;
 Alter table students
-add Column date_of_birth DATE after age;
+add Column date_of_birth DATE after age;--Adding new Column in a specific place
 
 use Chapter_2;
 select * from students
+
+use Chapter_2;
+alter table students
+MODIFY column age TINYINT UNSIGNED -- we use UNSIGNED to get all +ve vals
+
+
+USE Chapter_2;
+
+DESCRIBE students; --Describe is used to get the description of all columns
+
 
