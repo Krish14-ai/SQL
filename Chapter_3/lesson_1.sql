05b737ac4ee1cfc249ff42d72360f8feb7fc8b5b
krish14-ai <kkhandelwal292@gmail.com>
2026-09-05 23:45:09 +0530

    On Transaction

--- Chapter_3/lesson_1.sql
+++ Chapter_3/lesson_1.sql
@@ -1,1 +1,8 @@
--- Transactions :- In MySQL one or More SQL statements that are treated as one unit of work is Treated as One Transaction 
+-- Transactions :- In MySQL one or More SQL statements that are treated as 
+--one unit of work is Treated as One Transaction 
+
+-- To save Transactions Automatically
+set Autocommit = On 
+
+-- To Save it Manually
+commit
